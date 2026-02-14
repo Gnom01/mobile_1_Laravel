916f0ecee20c99e99a1ca238804768a615a70997
eugeniusz.scura@gmail.com <evgheni.scura@euvic.pl>
2026-02-14 12:21:33 +0100

    Poprawka

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -7,3 +7,10 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+use Illuminate\Support\Facades\Schedule;
+use App\Jobs\PullPaymentsJob;
+
+if (env('SYNC_ENABLED', false)) {
+    Schedule::job(new PullPaymentsJob())->everyFiveMinutes();
+}
