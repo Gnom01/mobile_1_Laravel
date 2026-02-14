0a0c02a099284576327be03151b234e3e6f678b5
eugeniusz.scura@gmail.com <evgheni.scura@euvic.pl>
2026-02-14 12:58:52 +0100

    Poprawka kernera

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -2,15 +2,21 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\Log;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-use Illuminate\Support\Facades\Schedule;
-use App\Jobs\PullPaymentsJob;
+Log::info('[CONSOLE.PHP] routes/console.php loaded - registering crm:sync schedule');
 
-if (env('SYNC_ENABLED', false)) {
-    Schedule::job(new PullPaymentsJob())->everyFiveMinutes();
-}
+Schedule::command('crm:sync')
+    ->everyFiveMinutes()
+    ->withoutOverlapping()
+    ->before(function () {
+        Log::info('[SCHEDULER] crm:sync is about to start');
+    })
+    ->after(function () {
+        Log::info('[SCHEDULER] crm:sync has finished');
+    });
