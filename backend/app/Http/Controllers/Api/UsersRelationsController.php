809ab82c24e9c5ccc3d1038a40fc2477c94211a7
eugeniusz.scura@gmail.com <evgheni.scura@euvic.pl>
2026-02-17 21:06:56 +0100

    Dodałem

--- backend/app/Http/Controllers/Api/UsersRelationsController.php
+++ backend/app/Http/Controllers/Api/UsersRelationsController.php
@@ -29,7 +29,8 @@ class UsersRelationsController extends Controller
                     ->limit(1);
             })
             ->where('ur.Cancelled', 0)
-            ->select('u.fullName', 'u.FirstName', 'u.LastName', 'u.DateOfBirdth', 'u.Pesel', 'u.address', 'u.Phone', 'u.Email', 'u.guid', 'u.Street', 'u.Building', 'u.Flat', 'u.City', 'u.PostalCode')
+            ->select('u.fullName', 'u.FirstName', 'u.LastName', 'u.DateOfBirdth', 'u.Pesel', 'u.address', 
+            'u.Phone', 'u.Email', 'u.guid', 'u.Street', 'u.Building', 'u.Flat', 'u.City', 'u.PostalCode')
             ->get();
 
         return response()->json($relatedUsers);
