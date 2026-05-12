eb18b23da1527e3f6427210d79452ee8cc9830d0
eugeniusz.scura@gmail.com <evgheni.scura@euvic.pl>
2026-05-12 14:33:27 +0200

    poprawka sych

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -18,7 +18,7 @@ class AuthController
     public function login(Request $request)
     {
 
-        // \App\Jobs\PullPriceListsTemplatesJob::dispatchSync();
+         \App\Jobs\PullProductsPaymentInstallmentsJob::dispatchSync();
         // PullDictionariesJob::PullCoursesJob(); // trigger sync to update dictionaries immediately after login 
 
         $request->validate([
